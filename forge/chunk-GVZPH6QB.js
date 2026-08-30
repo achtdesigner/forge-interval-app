e0b5f1f54f9851bde0fd0d4b55e65b5499b7723a
Sven <sprigge2013@gmail.com>
2026-08-30 22:02:44 +0200

    update forge

--- forge/chunk-GVZPH6QB.js
+++
@@ -1,1 +0,0 @@
-import{d as r,j as e,k as f,m}from"./chunk-APR4P74F.js";import{e as o}from"./chunk-IUWJPDSS.js";var p=o(()=>{"use strict";e();f();m();r()});export{p as a};
---
+++ forge/chunk-WPNWZLI2.js
@@ -0,0 +1,1 @@
+import{e as E}from"./chunk-IUWJPDSS.js";var L=E(()=>{"use strict"});export{L as a};
